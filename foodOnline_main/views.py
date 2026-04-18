04553635a428025089c65db862a64f31c4064845
Khushi Yadav <yadavkhushi0103@gmail.com>
2026-04-18 14:43:30 +0530

    add template and Created a new homepage template with structured layout, navigation, and search functionality.Integrated Bootstrap for responsive design and included necessary CSS and JS files.Installed postgresql and create database.

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, welcome to the food online store!")
+    return render(request, 'home.html')
